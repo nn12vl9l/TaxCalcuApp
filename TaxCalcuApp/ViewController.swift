83ba19065fe34f872d265cb8c8220c389b3fcbc7
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-11-08 16:20:58 +0900

    Initial Commit

---
+++ TaxCalcuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxCalcuApp
+//
+//  Created by 小倉瑞希 on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
